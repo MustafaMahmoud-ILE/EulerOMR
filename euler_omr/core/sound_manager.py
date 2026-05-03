f0d32f25cecc0a5b1ffd76788b963d1796e40dec
Mustafa Mahmoud <mustafa@eui.edu.eg>
2026-05-03 13:20:41 +0300

    SoundManager re-engineered to allow overlapping and uninterrupted playback

--- euler_omr/core/sound_manager.py
+++ euler_omr/core/sound_manager.py
@@ -3,23 +3,27 @@ import os
 from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput
 from PySide6.QtCore import QUrl
 
+
 class SoundManager:
-    _player = None
-    _audio_output = None
+    _active_players = []
 
     @classmethod
     def _play_sound(cls, filename: str):
-        # Path relative to euler_omr/core/sound_manager.py: up two levels to euler_omr root, up one more to project root
+        # Path relative to project root
         base_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-        sound_path = os.path.join(base_dir, "assets", "sounds", filename)
+        sound_path = os.path.abspath(os.path.join(base_dir, "assets", "sounds", filename))
         if os.path.exists(sound_path):
-            if cls._player is None:
-                cls._player = QMediaPlayer()
-                cls._audio_output = QAudioOutput()
-                cls._player.setAudioOutput(cls._audio_output)
-                cls._audio_output.setVolume(1.0)
-            cls._player.setSource(QUrl.fromLocalFile(sound_path))
-            cls._player.play()
+            player = QMediaPlayer()
+            audio_output = QAudioOutput()
+            player.setAudioOutput(audio_output)
+            audio_output.setVolume(1.0)
+            player.setSource(QUrl.fromLocalFile(sound_path))
+            cls._active_players.append((player, audio_output))
+            player.play()
+
+            # Trim list to avoid leaks while keeping alive
+            if len(cls._active_players) > 12:
+                cls._active_players.pop(0)
 
     @classmethod
     def play_open(cls):
